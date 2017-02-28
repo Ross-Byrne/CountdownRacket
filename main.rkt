7bda0bf98e5e7d464d8b03bbed93ab67a8639a8e
Ross Byrne <ross.byrne@outlook.ie>
2017-02-28 17:21:38 +0000

    Defined the list of allowed numbers

--- main.rkt
+++ main.rkt
@@ -1,3 +1,10 @@
 #lang racket
 
 ; The Count Down Numbers Game
+
+; define the list of allows numbers, of which 6 are randomly selected
+(define allowedNums (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
+
+; display allowed numbers
+"Allowed Numbers"
+allowedNums
