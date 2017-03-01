e3058ceba15e075d740084a0025233242046368d
Ross Byrne <ross.byrne@outlook.ie>
2017-03-01 15:29:08 +0000

    Working on evaluating RPN

---
+++ RPN.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+; Getting the hang of Reverse Polish Notation
+
+; Example Equation: (3 + 8) * 3
+
+; Racket version
+(* (+ 3 8) 3)
+
+; Reverse polish notation version
+; (38+3*)
+
+; Function to evaluate reverse polish notation
+;()
