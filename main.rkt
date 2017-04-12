f385f0b8c98bbce4599c62a9914d682351226b92
= <=>
2017-04-12 12:54:28 +0100

    Created list of all combinations of 6 numbers and 5 operators

--- main.rkt
+++ main.rkt
@@ -3,6 +3,9 @@
 ; cut out one -1 and two 1s
 (define start-perm (list -1 -1 -1 -1 1 1 1 1))
 
+; List of all operators that can be used
+(define operators (list '+ '- '* '/))
+
 ; Permutations must have 1 1 at the start. Must end with -1.
 (define x (remove-duplicates (permutations start-perm)))
 
@@ -13,15 +16,22 @@
 ; add 1 1 to start of all permutations. add -1 to the end of all permutations
 (define all-rpn-patterns (map make-rpn x))
 
+; define list of all possible operator combinations
+; using cartesian product
+(define all-5-operators (cartesian-product operators operators operators operators operators))
+
 ; Define a list of all perms of 6 numbers
 ; Will hard code 6 numbers for now
 (define number-list (list 1 2 2 4 1 6))
 
+; define a list of all perms of a 6 number list without dupes
+(define all-6-numbers (remove-duplicates (permutations number-list))) ; removes the dupes
+
 ;(length (permutations L)) ; without removing dupes
 (length (remove-duplicates (permutations number-list))) ; removes the dupes
 
-; define list of all possible operator combinations
-; Do using qartisan product
+; all of the possible combinations of the 5 operators
+(length all-5-operators)
 
 ; function that evaluates if rpn is valid.
 ; for every operator, there must be 2 nums on stack.
@@ -41,6 +51,10 @@
 
 ; display iist of valid rpn patterns
 ;valid-rpn-list
+
+; define a list of all the combinations of 5 operators
+; and 6 numbers
+(length (cartesian-product all-5-operators all-6-numbers))
 
 
 
