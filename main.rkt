c045e759560d21481c3920a836982c6f30e8da75
= <=>
2017-04-12 12:11:07 +0100

    Saved current work to racket file main.rkt

--- main.rkt
+++ main.rkt
@@ -1,10 +1,35 @@
 #lang racket
 
-; The Count Down Numbers Game
+; cut out one -1 and two 1s
+(define start-perm (list -1 -1 -1 -1 1 1 1 1))
 
-; define the list of allows numbers, of which 6 are randomly selected
-(define allowedNums (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
+; Permutations must have 1 1 at the start. Must end with -1.
+(define x (remove-duplicates (permutations start-perm)))
 
-; display allowed numbers
-"Allowed Numbers"
-allowedNums
+; function that adds 11 to the start and -1 to the end of all permutations
+(define (make-rpn l)
+  (append (list 1 1) l (list -1)))
+
+; add 1 1 to start of all permutations. add -1 to the end of all permutations
+(define all-rpn-patterns (map make-rpn x))
+
+; function that evaluates if rpn is valid.
+; for every operator, there must be 2 nums on stack.
+; at the end, only one num should be on stack.
+(define (valid-rpn? e [s 0])
+  (if (null? e)
+      (if (= s 1) #t #f)
+      (if (= (car e) 1)
+          (valid-rpn? (cdr e) (+ 1 s)) ; if number, add one to stack and pass cdr of list into func again
+          (valid-rpn? (cdr e) (- 1 s)) ; if operator, take one from stack and pass cdr of list into func again
+          )))
+
+; filters all of the possible rpn patterns
+; runs each one through valid-rpn? function
+; and only returns the pattern if it is valid (if valid-rpn? returns #t)
+(define valid-rpn-list (filter (lambda (l) (equal? (valid-rpn? l) #t)) all-rpn-patterns))
+
+; display iist of valid rpn patterns
+valid-rpn-list
+
+
